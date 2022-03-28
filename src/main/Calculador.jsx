db3ee09a88526dda5e372db4fe098bdf5ec19d40
Victor Torres <torresvictor100@gmail.com>
2022-03-28 08:45:01 -0300

    componets e css da classe calculador pronto

---
+++ src/main/Calculador.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./Calculator.css"
+import Button from '../components/Button'
+import Display from "../components/Display";
+
+export default class Calculator extends Component {
+
+    
+}
